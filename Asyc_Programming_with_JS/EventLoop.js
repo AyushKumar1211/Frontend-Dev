13b8d835106a204aee4c815e5e775ca28c7f0f0f
Ayush Kumar <ayushkumar121105@gmail.com>
2025-11-23 12:47:56 +0530

    Adding: Question 9

---
+++ Asyc_Programming_with_JS/EventLoop.js
@@ -0,0 +1,16 @@
+// Predicted output:
+// Script start
+// Script end
+// Promise callback
+// Timeout callback
+console.log("Script start");
+
+setTimeout(() => {
+    console.log("Timeout callback");
+}, 0);
+
+Promise.resolve().then(() => {
+    console.log("Promise callback");
+});
+
+console.log("Script end");
